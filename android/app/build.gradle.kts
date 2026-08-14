1587800e35a671c51c31f3f257a32f6217b76a51
Nadeena-Srioshi <nadshrio@gmail.com>
2026-08-14 08:10:53 +0530

    fix: update ndkVersion to use flutter.ndkVersion and add .env asset

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 android {
     namespace = "com.greenyield.greenyield"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = "27.3.13750724"
+    ndkVersion = "flutter.ndkVersion"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
